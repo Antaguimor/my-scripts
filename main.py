cb522843f8b03ec9c80002d9ca08d4a0951fdade
Antonio Aguilar <aaguilar@ventus.group>
2021-01-18 10:11:14 +0100

    Version 01

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# Prueba 021
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    nombre = input ("Introduce name: ")
+    print_hi(nombre)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
